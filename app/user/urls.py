d98415c198d7b6376943ce72402683f9f65c9d6e
Diego Alliende <diego.alliende@magnet.cl>
2020-05-21 22:17:56 -0400

    Add create and obtain token user

---
+++ app/user/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from user.views import CreateUserView
+from user.views import CreateTokenView
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path(
+        'create/',
+        CreateUserView.as_view(),
+        name='create',
+    ),
+    path(
+        'token/',
+        CreateTokenView.as_view(),
+        name='token',
+    ),
+]
